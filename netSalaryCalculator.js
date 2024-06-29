cd0dd980bcccfbdec08b88cbdb7edc443ea08a52
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-06-29 17:20:24 +0300

    Changed if-else statements to ternary

--- netSalaryCalculator.js
+++ netSalaryCalculator.js
@@ -1,70 +1,40 @@
 function getNetIncome(grossIncome){
-
     if (typeof grossIncome !== 'number') {
         console.log('Invalid input');
+
     } else {
-        
         // Calculating the PAYE per month
-        var kra = undefined
-        if(grossIncome <= 24000){
-            kra = grossIncome * .1
-        } else if(grossIncome > 24000 && grossIncome <= 32333){
-            kra = grossIncome * .25
-        } else if(grossIncome > 32333 && grossIncome <= 500000){
-            kra = grossIncome * .30
-        } else if(grossIncome > 500000 && grossIncome <= 800000){
-            kra = grossIncome * .325
-        } else {
-            kra = grossIncome * .35
-        }
+        var kra =
+            grossIncome <= 24000 ? grossIncome * 0.1 :
+            grossIncome <= 32333 ? grossIncome * 0.25 :
+            grossIncome <= 500000 ? grossIncome * 0.30 :
+            grossIncome <= 800000 ? grossIncome * 0.325 :
+            grossIncome * 0.35; // Default grossIncome is above 800,000
 
         // Calculating NHIF deductions
-        var nhif = undefined
-        if(grossIncome <= 5999){
-            nhif = 150
-        } else if(grossIncome > 5999 && grossIncome <= 7999){
-            nhif = 300
-        } else if(grossIncome > 7999 && grossIncome <= 11999){
-            nhif = 400
-        } else if(grossIncome > 11999 && grossIncome <= 14999){
-            nhif = 500
-        } else if(grossIncome > 14999 && grossIncome <= 19999){
-            nhif = 600
-        } else if(grossIncome > 19999 && grossIncome <= 24999){
-            nhif = 750
-        } else if(grossIncome > 24999 && grossIncome <= 29999){
-            nhif = 850
-        } else if(grossIncome > 29999 && grossIncome <= 34999){
-            nhif = 900
-        } else if(grossIncome > 34999 && grossIncome <= 39999){
-            nhif = 950
-        } else if(grossIncome > 39999 && grossIncome <= 44999){
-            nhif = 1000
-        } else if(grossIncome > 44999 && grossIncome <= 49999){
-            nhif = 1100
-        } else if(grossIncome > 49999 && grossIncome <= 59999){
-            nhif = 1200
-        } else if(grossIncome > 59999 && grossIncome <= 69999){
-            nhif = 1300
-        } else if(grossIncome > 69999 && grossIncome <= 79999){
-            nhif = 1400
-        } else if(grossIncome > 79999 && grossIncome <= 89999){
-            nhif = 1500
-        } else if(grossIncome > 89999 && grossIncome <= 99999){
-            nhif = 1600
-        } else if(grossIncome > 99999){
-            nhif = 1700
-        }
+        var nhif =
+            grossIncome <= 5999 ? 150 :
+            grossIncome <= 7999 ? 300 :
+            grossIncome <= 11999 ? 400 :
+            grossIncome <= 14999 ? 500 :
+            grossIncome <= 19999 ? 600 :
+            grossIncome <= 24999 ? 750 :
+            grossIncome <= 29999 ? 850 :
+            grossIncome <= 34999 ? 900 :
+            grossIncome <= 39999 ? 950 :
+            grossIncome <= 44999 ? 1000 :
+            grossIncome <= 49999 ? 1100 :
+            grossIncome <= 59999 ? 1200 :
+            grossIncome <= 69999 ? 1300 :
+            grossIncome <= 79999 ? 1400 :
+            grossIncome <= 89999 ? 1500 :
+            grossIncome <= 99999 ? 1600 :
+            1700; // Default value if grossIncome > 99999
 
         // Calculating NSSF deductions
-        var nssf = undefined;
-        if(grossIncome <= 7000){
-            nssf = .06 * 7000
-        } else {
-            nssf = .06 * 36000
-        }
+        var nssf = grossIncome <= 7000 ? 0.06 * 7000 : 0.06 * 36000;
 
-        // Return the values:
+        // Return the values of total PAYE and Net Income
         let paye = kra + nhif + nssf;
         let netIncome = grossIncome - paye
 
